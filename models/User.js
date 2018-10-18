5750a6c1597cb578848ec72982d99504c4bf0719
EC2 Default User <ec2-user@ip-172-31-26-149.ap-southeast-1.compute.internal>
2018-10-18 13:07:52 +0000

    index page prototype builded.

--- models/User.js
+++ models/User.js
@@ -11,6 +11,7 @@ User.add({
 	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
+	description: { type: Types.Text, initial: true, index: true },
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
 });
